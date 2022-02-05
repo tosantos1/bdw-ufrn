e4bbcb099d8309bed69d27ec1fc0393bc2a91a7c
Tiago Oliveira <tiagolivesan@outlook.com>
2022-02-04 21:49:04 -0300

    Finalizando o nosso App

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+  <title>BDW - Cadastro</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <img src="/image/logo.png" alt="Logo Tiago Oliveira">
+      <h1>Tiago Oliveira</h1>
+    </nav>
+  </header>
+  <main>
+    <h1> Login  </h1>
+    <br>
+    <form class="form" action="validar_usuario.php" method="Post">
+        E-mail:
+        <input type="text" name="email">
+        Senha
+        <input type="password" name="senha">
+        <input type="submit" value="Entrar">
+    </br>
+
+        <a href="index.php" class="bnt-voltar-cadastro" > Voltar </a>
+    </br>
+  </main>
+  
+  <footer>
+    <h1>Banco de dados para Web</h1>
+    <h2>Sobre</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates est incidunt tenetur ab, nemo sed quia odio
+      omnis eum cumque molestiae. Laborum a cum inventore et odit eveniet, expedita possimus.</p>
+    <div class="linguagens">
+      <h2>Tecnologias</h2>
+      <div class="imgstech">
+        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><img src="/image/HTML.png" alt="Logo HTML"></a>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><img src="/image/CSS.png" alt="Logo HTML"></a>
+
+
+      </div>
+    </div>
+    <h2>Referências</h2>
+    <div class="nameimg">
+      <a href="https://www.youtube.com/channel/UCWUVAPzYPj1sk6krbnyIgMg"> Aquiles Burlamaqui <img
+          src="/../../image/youtube.png" alt="Logo Youtube"></a>
+    </div>
+
+  </footer>
+
+   
+</body>
+
+</html>
